45a91e4fda86eb0df6c57d046777f8824d8a79b7
Matt Stroud <lixingke06@gmail.com>
2020-03-24 16:40:57 -0700

    add talk.html and js to prepend messages

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,19 @@ $(document).ready(function() {
     $(this).children(".definition").slideToggle(); 
     $(this).children(".word").slideToggle();
   });
+
+  $("button#hello").click(function() {
+    $("ul.user").prepend("<li>Hello!</li>");
+    $("ul.webpage").prepend("<li>Why hello there!</li>");
+  });
+
+    $("button#goodbye").click(function() {
+      $("ul.user").prepend("<li>Goodbye!</li>");
+      $("ul.ebpage").prepend("<li>Goodbye, dear user!</li>");
+    });
+  
+    $("button#stop").click(function() {
+      $("ul.user").prepend("<li>Stop copying me!</li>");
+      $("ul.webpage").prepend("<li>Pardon me. I meant no offense.</li>");
+    });
 });
